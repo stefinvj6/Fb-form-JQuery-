827c779f10a761a035486b1f6e937dd786ad0cb3
Stefin Vj <stefinvj6@gmail.com>
2023-06-11 21:13:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{            
+            fname:{
+                required:true,
+                minlength:3,
+                maxlength:10
+            },
+            sname:{
+                required:true,
+                minlength:4
+            },
+            emailAddress:{
+                required:true,
+                email:true
+            },
+            password:{
+                required:true,
+                minlength:6,             
+            },
+            confirmPassword:{
+                required:true,
+                minlength:6,
+                equalTo:"#password"
+            },
+            day:{
+                required:true
+            },
+            month:{
+                required:true
+            },
+            year:{
+                required:true
+            },
+            gender:{
+                required:true
+            }
+        },
+        messages:{
+            fname:{
+                required:"Enter Your First Name"
+            },
+            sname:{
+                required:"Enter Your Sir Name"
+            },
+            emailAddress:{
+                required:'Enter Your EmailAddress'
+            },
+            password:{
+                required:"Enter Your Password"
+            },
+        }
+    })
+})
